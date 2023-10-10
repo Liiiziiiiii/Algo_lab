56001175221ea24997f74246f32b17e946e36a54
Liiiziiiiii <119292426+Liiiziiiiii@users.noreply.github.com>
2023-10-10 18:48:47 +0300

    adding 2 lab

--- main.py
+++ main.py
@@ -1,46 +1,39 @@
-def peak_length(array):
-    n = len(array)
-    peaks = []
-    max_len_peak = 0
-    max_peak = []
-    peak = 0
+def func_cow(arr, count_cow):
+    arr.sort()
+    final_number = 0
+    number = 1
+    current_stable_with_cow = arr[0]
+    count_cow -= 1
+    copy_count_cow = count_cow
+    cow_arr = []
 
-    i = 0
-    while i < n:
-        left_peak = []
-        while i < n - 1 and array[i] < array[i + 1]:
-            left_peak.append(array[i])
-            i += 1
+    while number != arr[-1]:
+        if copy_count_cow == 0:
+            final_number = number
 
-        right_peak = []
+        copy_count_cow = count_cow
+        number += 1
+        # print("number", number)
+        i = 0
 
-        while i < n - 1 and array[i] > array[i + 1]:
-            right_peak.append(array[i])
-            i += 1
-            right_peak.append(array[i])
+        for i, value in enumerate(arr[:-1]):
+            if number <= arr[i + 1] - current_stable_with_cow:
+                current_stable_with_cow = arr[i + 1]
+                # print("current_stable_with_cow", current_stable_with_cow)
+                cow_arr.append(current_stable_with_cow)
+                # if len(cow_arr) == final_number+1:
+                #     print("cow_arr", cow_arr)
+                copy_count_cow -= 1
+                if copy_count_cow == 0:
+                    cow_arr.clear()
+                    cow_arr.append(arr[0])
+                    current_stable_with_cow = arr[0]
+                    break
 
-        if left_peak and right_peak:
-            all_side_peak = left_peak + right_peak
-            peaks.append(all_side_peak)
-            if len(all_side_peak) > max_len_peak:
-                max_len_peak = len(all_side_peak)
-                max_peak = all_side_peak
-
-        i += 1
-
-        # peak = max(max_peak)
-
-        return max_len_peak
-
-    # return max_len_peak, # return peaks, "    ", peak, "    ", max_len_peak, "   ", max_peak
+    print(final_number)
+    return final_number
 
 
-print(peak_length([2, 3, 4, 5, 0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5, 0]))
-# print(peak_length([1, 3, 5, 4, 2, 0, 8, 3, 7]))
-
-# print(peak_length([-4, -3, -2, -1, -8, 8]))
-
-# [1, 3, 8, 9, 0]
-# [1, 4, 8, 11, 12, 1]
-# [2, 8, 9, 0]
-# 6
+func_cow([1, 2, 3, 4, 5, 10, 30, 40, 60, 90], 4   )
+func_cow([1, 2, 8, 4, 9], 3)
+func_cow([2, 5, 7, 11, 15, 20], 3)
